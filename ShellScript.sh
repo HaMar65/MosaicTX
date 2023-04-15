f319aa0fdc2c7efa5fa6eb874eeaa60c19e1713a
HaMar65 <130861353+HaMar65@users.noreply.github.com>
2023-04-15 21:00:35 +0100

    Update ShellScript.sh

--- ShellScript.sh
+++ ShellScript.sh
@@ -1,5 +1,5 @@
 # install R on ubunto
-Sudo apt install r-base-core
+sudo apt install r-base-core
 # Install R package
 Rscript -e 'install.packages("devtools",path=.libPath()[1])'
 Rscript -e 'library(devtools);install_github("HaMar65/MosaicTX")'
