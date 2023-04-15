5c34f8a22e007d06a741b06b8d11096d01b664c1
HaMar65 <130861353+HaMar65@users.noreply.github.com>
2023-04-15 20:59:05 +0100

    Update ShellScript.sh

--- ShellScript.sh
+++ ShellScript.sh
@@ -1,6 +1,6 @@
 # install R on ubunto
 Sudo apt install r-base-core
 # Install R package
-Rscript -e 'install.packages("devtools")'
+Rscript -e 'install.packages("devtools",path=.libPath()[1])'
 Rscript -e 'library(devtools);install_github("HaMar65/MosaicTX")'
 Rscript -e 'library(MosaicTX);example(CalculateAssociationsTable)'
