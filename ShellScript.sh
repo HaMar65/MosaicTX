643f4ae144c0a9cf2f4d9be70071af4287374dde
HaMar65 <130861353+HaMar65@users.noreply.github.com>
2023-04-15 21:07:32 +0100

    Update ShellScript.sh

--- ShellScript.sh
+++ ShellScript.sh
@@ -1,9 +1,10 @@
-# install R on ubunto
+% install R on ubunto
+sudo apt update
 sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
 sudo apt install r-base
-# Install R package
+% Install R package
 Rscript -e 'install.packages("devtools",path=.libPath()[1])'
 Rscript -e 'library(devtools);install_github("HaMar65/MosaicTX")'
 Rscript -e 'library(MosaicTX);example(CalculateAssociationsTable)'
